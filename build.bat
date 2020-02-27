58fcec63b492aa11532c5d6f486f5f2e148001b6
Vivek Hari <vivek.hari@outlook.co.nz>
2020-02-27 19:34:14 +1300

    Use blue background colour, close window when ESC is pressed.

--- build.bat
+++ build.bat
@@ -2,14 +2,16 @@
 
 set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat"
 set PLATFORM=x64
-set SRC_FILE=src\main.cpp
+
+set SRC_FILES=src\main.cpp src\lib\glad.c
+
+set INCLUDES=/I deps /I deps\nuget\glfw.3.3.2\build\native\include
+set LIB_PATH=deps\nuget\glfw.3.3.2\build\native\lib\static\v142\x64
+set LIBS=glfw3.lib user32.lib gdi32.lib shell32.lib
+
 set OBJ_DIR=obj
 set BIN_DIR=bin
 set EXE_FILENAME=terrain.exe
-
-set INCLUDE_PATH=deps\nuget\glfw.3.3.2\build\native\include
-set LIB_PATH=deps\nuget\glfw.3.3.2\build\native\lib\static\v142\x64
-set LIBS=glfw3.lib user32.lib gdi32.lib shell32.lib
 
 echo Initializing environment...
 call %VCVARSALL% %PLATFORM%
@@ -24,6 +26,6 @@ echo Restore packages...
 nuget restore deps\packages.config
 
 echo Building...
-cl /MD /EHsc %SRC_FILE% /Fo%OBJ_DIR%/ /I %INCLUDE_PATH% /link /out:%BIN_DIR%\%EXE_FILENAME% /LIBPATH:%LIB_PATH% %LIBS%
+cl /MD /EHsc %SRC_FILES% /Fo%OBJ_DIR%/ %INCLUDES% /link /out:%BIN_DIR%\%EXE_FILENAME% /LIBPATH:%LIB_PATH% %LIBS%
 
 echo Done.
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int run();
+void onFrameBufferResized(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int run(int width, int height)
+{
+    auto *window = glfwCreateWindow(width, height, "Terrain", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, width, height);
+    glfwSetFramebufferSizeCallback(window, onFrameBufferResized);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.392f, 0.584f, 0.929f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    return 0;
+}
 
 int main()
 {
@@ -10,27 +55,8 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    int exitCode = run();
+    int exitCode = run(800, 600);
 
     glfwTerminate();
     return exitCode;
-}
-
-int run()
-{
-    auto *window = glfwCreateWindow(800, 600, "Terrain", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    return 0;
 }
