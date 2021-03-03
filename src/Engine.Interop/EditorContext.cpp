843b37a5eb7e271079e513cddf666a879a540f72
Vivek Hari <vivek.hari@outlook.co.nz>
2021-03-04 00:31:55 +1300

    Editor UI - only respond to key presses while window is focused.

--- src/Engine.Interop/EditorContext.cpp
+++ src/Engine.Interop/EditorContext.cpp
@@ -128,73 +128,76 @@ namespace Terrain { namespace Engine { namespace Interop {
             }
 
             // update keyboard state for hovered viewport
-            unsigned long long &pressedKeys = inputState.pressedKeys[inputControllerId];
+            if (appWindow->IsKeyboardFocusWithin)
+            {
+                unsigned long long &pressedKeys = inputState.pressedKeys[inputControllerId];
 
 #define UPDATE_KEYBOARD_STATE(ENGINE_KEY, WINDOWS_KEY)                                        \
     pressedKeys |= Keyboard::IsKeyDown(WINDOWS_KEY)                                           \
-        * static_cast<unsigned long long>(Terrain::Engine::IO::Key::ENGINE_KEY);
+        * static_cast<unsigned long long>(Terrain::Engine::IO::Key::ENGINE_KEY)
 
-            UPDATE_KEYBOARD_STATE(Space, System::Windows::Input::Key::Space)
-            UPDATE_KEYBOARD_STATE(D0, System::Windows::Input::Key::D0)
-            UPDATE_KEYBOARD_STATE(D1, System::Windows::Input::Key::D1)
-            UPDATE_KEYBOARD_STATE(D2, System::Windows::Input::Key::D2)
-            UPDATE_KEYBOARD_STATE(D3, System::Windows::Input::Key::D3)
-            UPDATE_KEYBOARD_STATE(D4, System::Windows::Input::Key::D4)
-            UPDATE_KEYBOARD_STATE(D5, System::Windows::Input::Key::D5)
-            UPDATE_KEYBOARD_STATE(D6, System::Windows::Input::Key::D6)
-            UPDATE_KEYBOARD_STATE(D7, System::Windows::Input::Key::D7)
-            UPDATE_KEYBOARD_STATE(D8, System::Windows::Input::Key::D8)
-            UPDATE_KEYBOARD_STATE(D9, System::Windows::Input::Key::D9)
-            UPDATE_KEYBOARD_STATE(A, System::Windows::Input::Key::A)
-            UPDATE_KEYBOARD_STATE(B, System::Windows::Input::Key::B)
-            UPDATE_KEYBOARD_STATE(C, System::Windows::Input::Key::C)
-            UPDATE_KEYBOARD_STATE(D, System::Windows::Input::Key::D)
-            UPDATE_KEYBOARD_STATE(E, System::Windows::Input::Key::E)
-            UPDATE_KEYBOARD_STATE(F, System::Windows::Input::Key::F)
-            UPDATE_KEYBOARD_STATE(G, System::Windows::Input::Key::G)
-            UPDATE_KEYBOARD_STATE(H, System::Windows::Input::Key::H)
-            UPDATE_KEYBOARD_STATE(I, System::Windows::Input::Key::I)
-            UPDATE_KEYBOARD_STATE(J, System::Windows::Input::Key::J)
-            UPDATE_KEYBOARD_STATE(K, System::Windows::Input::Key::K)
-            UPDATE_KEYBOARD_STATE(L, System::Windows::Input::Key::L)
-            UPDATE_KEYBOARD_STATE(M, System::Windows::Input::Key::M)
-            UPDATE_KEYBOARD_STATE(N, System::Windows::Input::Key::N)
-            UPDATE_KEYBOARD_STATE(O, System::Windows::Input::Key::O)
-            UPDATE_KEYBOARD_STATE(P, System::Windows::Input::Key::P)
-            UPDATE_KEYBOARD_STATE(Q, System::Windows::Input::Key::Q)
-            UPDATE_KEYBOARD_STATE(R, System::Windows::Input::Key::R)
-            UPDATE_KEYBOARD_STATE(S, System::Windows::Input::Key::S)
-            UPDATE_KEYBOARD_STATE(T, System::Windows::Input::Key::T)
-            UPDATE_KEYBOARD_STATE(U, System::Windows::Input::Key::U)
-            UPDATE_KEYBOARD_STATE(V, System::Windows::Input::Key::V)
-            UPDATE_KEYBOARD_STATE(W, System::Windows::Input::Key::W)
-            UPDATE_KEYBOARD_STATE(X, System::Windows::Input::Key::X)
-            UPDATE_KEYBOARD_STATE(Y, System::Windows::Input::Key::Y)
-            UPDATE_KEYBOARD_STATE(Z, System::Windows::Input::Key::Z)
-            UPDATE_KEYBOARD_STATE(Escape, System::Windows::Input::Key::Escape)
-            UPDATE_KEYBOARD_STATE(Enter, System::Windows::Input::Key::Enter)
-            UPDATE_KEYBOARD_STATE(Right, System::Windows::Input::Key::Right)
-            UPDATE_KEYBOARD_STATE(Left, System::Windows::Input::Key::Left)
-            UPDATE_KEYBOARD_STATE(Down, System::Windows::Input::Key::Down)
-            UPDATE_KEYBOARD_STATE(Up, System::Windows::Input::Key::Up)
-            UPDATE_KEYBOARD_STATE(F1, System::Windows::Input::Key::F1)
-            UPDATE_KEYBOARD_STATE(F2, System::Windows::Input::Key::F2)
-            UPDATE_KEYBOARD_STATE(F3, System::Windows::Input::Key::F3)
-            UPDATE_KEYBOARD_STATE(F4, System::Windows::Input::Key::F4)
-            UPDATE_KEYBOARD_STATE(F5, System::Windows::Input::Key::F5)
-            UPDATE_KEYBOARD_STATE(F6, System::Windows::Input::Key::F6)
-            UPDATE_KEYBOARD_STATE(F7, System::Windows::Input::Key::F7)
-            UPDATE_KEYBOARD_STATE(F8, System::Windows::Input::Key::F8)
-            UPDATE_KEYBOARD_STATE(F9, System::Windows::Input::Key::F9)
-            UPDATE_KEYBOARD_STATE(F10, System::Windows::Input::Key::F10)
-            UPDATE_KEYBOARD_STATE(F11, System::Windows::Input::Key::F11)
-            UPDATE_KEYBOARD_STATE(F12, System::Windows::Input::Key::F12)
-            UPDATE_KEYBOARD_STATE(LeftShift, System::Windows::Input::Key::LeftShift)
-            UPDATE_KEYBOARD_STATE(LeftCtrl, System::Windows::Input::Key::LeftCtrl)
-            UPDATE_KEYBOARD_STATE(LeftAlt, System::Windows::Input::Key::LeftAlt)
-            UPDATE_KEYBOARD_STATE(RightShift, System::Windows::Input::Key::RightShift)
-            UPDATE_KEYBOARD_STATE(RightCtrl, System::Windows::Input::Key::RightCtrl)
-            UPDATE_KEYBOARD_STATE(RightAlt, System::Windows::Input::Key::RightAlt)
+                UPDATE_KEYBOARD_STATE(Space, System::Windows::Input::Key::Space);
+                UPDATE_KEYBOARD_STATE(D0, System::Windows::Input::Key::D0);
+                UPDATE_KEYBOARD_STATE(D1, System::Windows::Input::Key::D1);
+                UPDATE_KEYBOARD_STATE(D2, System::Windows::Input::Key::D2);
+                UPDATE_KEYBOARD_STATE(D3, System::Windows::Input::Key::D3);
+                UPDATE_KEYBOARD_STATE(D4, System::Windows::Input::Key::D4);
+                UPDATE_KEYBOARD_STATE(D5, System::Windows::Input::Key::D5);
+                UPDATE_KEYBOARD_STATE(D6, System::Windows::Input::Key::D6);
+                UPDATE_KEYBOARD_STATE(D7, System::Windows::Input::Key::D7);
+                UPDATE_KEYBOARD_STATE(D8, System::Windows::Input::Key::D8);
+                UPDATE_KEYBOARD_STATE(D9, System::Windows::Input::Key::D9);
+                UPDATE_KEYBOARD_STATE(A, System::Windows::Input::Key::A);
+                UPDATE_KEYBOARD_STATE(B, System::Windows::Input::Key::B);
+                UPDATE_KEYBOARD_STATE(C, System::Windows::Input::Key::C);
+                UPDATE_KEYBOARD_STATE(D, System::Windows::Input::Key::D);
+                UPDATE_KEYBOARD_STATE(E, System::Windows::Input::Key::E);
+                UPDATE_KEYBOARD_STATE(F, System::Windows::Input::Key::F);
+                UPDATE_KEYBOARD_STATE(G, System::Windows::Input::Key::G);
+                UPDATE_KEYBOARD_STATE(H, System::Windows::Input::Key::H);
+                UPDATE_KEYBOARD_STATE(I, System::Windows::Input::Key::I);
+                UPDATE_KEYBOARD_STATE(J, System::Windows::Input::Key::J);
+                UPDATE_KEYBOARD_STATE(K, System::Windows::Input::Key::K);
+                UPDATE_KEYBOARD_STATE(L, System::Windows::Input::Key::L);
+                UPDATE_KEYBOARD_STATE(M, System::Windows::Input::Key::M);
+                UPDATE_KEYBOARD_STATE(N, System::Windows::Input::Key::N);
+                UPDATE_KEYBOARD_STATE(O, System::Windows::Input::Key::O);
+                UPDATE_KEYBOARD_STATE(P, System::Windows::Input::Key::P);
+                UPDATE_KEYBOARD_STATE(Q, System::Windows::Input::Key::Q);
+                UPDATE_KEYBOARD_STATE(R, System::Windows::Input::Key::R);
+                UPDATE_KEYBOARD_STATE(S, System::Windows::Input::Key::S);
+                UPDATE_KEYBOARD_STATE(T, System::Windows::Input::Key::T);
+                UPDATE_KEYBOARD_STATE(U, System::Windows::Input::Key::U);
+                UPDATE_KEYBOARD_STATE(V, System::Windows::Input::Key::V);
+                UPDATE_KEYBOARD_STATE(W, System::Windows::Input::Key::W);
+                UPDATE_KEYBOARD_STATE(X, System::Windows::Input::Key::X);
+                UPDATE_KEYBOARD_STATE(Y, System::Windows::Input::Key::Y);
+                UPDATE_KEYBOARD_STATE(Z, System::Windows::Input::Key::Z);
+                UPDATE_KEYBOARD_STATE(Escape, System::Windows::Input::Key::Escape);
+                UPDATE_KEYBOARD_STATE(Enter, System::Windows::Input::Key::Enter);
+                UPDATE_KEYBOARD_STATE(Right, System::Windows::Input::Key::Right);
+                UPDATE_KEYBOARD_STATE(Left, System::Windows::Input::Key::Left);
+                UPDATE_KEYBOARD_STATE(Down, System::Windows::Input::Key::Down);
+                UPDATE_KEYBOARD_STATE(Up, System::Windows::Input::Key::Up);
+                UPDATE_KEYBOARD_STATE(F1, System::Windows::Input::Key::F1);
+                UPDATE_KEYBOARD_STATE(F2, System::Windows::Input::Key::F2);
+                UPDATE_KEYBOARD_STATE(F3, System::Windows::Input::Key::F3);
+                UPDATE_KEYBOARD_STATE(F4, System::Windows::Input::Key::F4);
+                UPDATE_KEYBOARD_STATE(F5, System::Windows::Input::Key::F5);
+                UPDATE_KEYBOARD_STATE(F6, System::Windows::Input::Key::F6);
+                UPDATE_KEYBOARD_STATE(F7, System::Windows::Input::Key::F7);
+                UPDATE_KEYBOARD_STATE(F8, System::Windows::Input::Key::F8);
+                UPDATE_KEYBOARD_STATE(F9, System::Windows::Input::Key::F9);
+                UPDATE_KEYBOARD_STATE(F10, System::Windows::Input::Key::F10);
+                UPDATE_KEYBOARD_STATE(F11, System::Windows::Input::Key::F11);
+                UPDATE_KEYBOARD_STATE(F12, System::Windows::Input::Key::F12);
+                UPDATE_KEYBOARD_STATE(LeftShift, System::Windows::Input::Key::LeftShift);
+                UPDATE_KEYBOARD_STATE(LeftCtrl, System::Windows::Input::Key::LeftCtrl);
+                UPDATE_KEYBOARD_STATE(LeftAlt, System::Windows::Input::Key::LeftAlt);
+                UPDATE_KEYBOARD_STATE(RightShift, System::Windows::Input::Key::RightShift);
+                UPDATE_KEYBOARD_STATE(RightCtrl, System::Windows::Input::Key::RightCtrl);
+                UPDATE_KEYBOARD_STATE(RightAlt, System::Windows::Input::Key::RightAlt);
+            }
         }
 
         // reset the mouse scroll offset that will be modified by the scroll wheel callback
